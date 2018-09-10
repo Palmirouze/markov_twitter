993f1640b5724d27443dabdf3c0ebd615228041c
paulpalmieri <paulpalmieri@outlook.fr>
2018-09-10 12:00:46 -0400

    Added comments and better structure for main file

--- generator.py
+++ generator.py
@@ -2,17 +2,15 @@ import tweepy
 import markovify
 import json
 
-consumer_key = ''
-consumer_secret = ''
-access_key = ''
-access_secret = ''
-
+# twitter API throttle
 MAX_TIMELINE_POSTS = 200
 
+# parse credentials and target account
 with open("twitter-credentials.json") as f:
     credentials = json.load(f)
 
-#gets the timeline of a twitter user, 200 tweets at a time, for a choosen number of pages
+# gets the entire timeline of a twitter user
+# Twitter prevents entire user scraping so the trick is to do it 200 tweets at a time for a choosen number of pages
 def get_tweets(n_page, twitter_user_name):
 
     auth = tweepy.OAuthHandler(credentials["consumer_key"], credentials["consumer_secret"])
@@ -30,8 +28,7 @@ def get_tweets(n_page, twitter_user_name):
     return tweets
 
 
-#todo: decide if mentions should be included
-# removes a bunch of stuff that we don't want to input inside the markov chain
+# removes a bunch data we don't want to input to the Markov model
 def trim_tweets(tweet):
     processed_text = []
 
@@ -50,12 +47,15 @@ def print_markov_chain_tweet(text, n_tweet):
     for i in range(n_tweet):
         print(markov_model.make_short_sentence(160, 30))
 
+def main():
+    timeline = get_tweets(10, credentials["target_account"]) #get 20 pages of
+    processed_timeline = [trim_tweets(tweet) for tweet in timeline]
+    text_timeline = "\n".join(processed_timeline)
+    #print_markov_chain_tweet(text_timeline, 10)
+    markov_model = markovify.text.NewlineText(text_timeline)
 
-timeline = get_tweets(10, credentials["target_account"]) #get 20 pages of
-processed_timeline = [trim_tweets(tweet) for tweet in timeline]
-text_timeline = "\n".join(processed_timeline)
-#print_markov_chain_tweet(text_timeline, 10)
-markov_model = markovify.text.NewlineText(text_timeline)
+    for i in range(20):
+        print(markov_model.make_sentence(tries=10000))
 
-for i in range(20):
-    print(markov_model.make_sentence(tries=10000))
+if __name__ == '__main__':
+    main()
